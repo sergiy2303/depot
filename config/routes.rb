1a6d796d438a24b0dbcb3a5e5729d5de60c00454
Darya Kuritsyna <daryakuritsyna@gmail.com>
2015-11-10 14:39:12 +0200

    Email async confirmation (async adapter, sucker_punch) add boolean field confirmed default: false  token => MessageVerifier  action confirm in UsersController  Spec coverage 100%  Don't allow users to login until email confirmation Please confirm your email  Mirgate to slim

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   resource :user
   resource :session, only: [:new, :create, :destroy]
   root 'welcome#index'
+  resources :users do
+    member do
+      get :confirm_email
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102092344) do
+ActiveRecord::Schema.define(version: 20151109111200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,10 @@ ActiveRecord::Schema.define(version: 20151102092344) do
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "email_confirmed", default: false
+    t.string   "confirm_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
