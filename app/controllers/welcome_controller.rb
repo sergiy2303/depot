247760c2c746841456004f2b2a18febc0f61c40f
Denis Bereza <den.intellcom@gmail.com>
2015-11-22 20:11:58 +0200

    Add products and categories to root page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,20 @@
 class WelcomeController < ApplicationController
   before_action :require_guest
+  expose :categories
+
   def index
     @session = Session.new
+  end
+
+  private
+
+  def products
+    products = params[:category] ? category.products : Product.all
+    products.page(params[:page]).per(8)
+  end
+  helper_method :products
+
+  def category
+    Category.find(params[:category])
   end
 end
